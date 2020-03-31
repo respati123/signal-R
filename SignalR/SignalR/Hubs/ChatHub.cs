87165f5cecf98bd499130b908ecd0f19ba24c2e5
Ahmad Ilman Fadilah <ahmadilmanfadilah@gmail.com>
2020-04-01 06:43:52 +0700

    add net chathub

--- SignalR/SignalR/Hubs/ChatHub.cs
+++ SignalR/SignalR/Hubs/ChatHub.cs
@@ -1,17 +1,32 @@
-﻿using Microsoft.AspNetCore.SignalR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SignalR.Hubs
 {
     public class ChatHub : Hub
     {
-        
-        public async Task SendMessage(string user, string message)
+
+        public string GetConnectionId()
         {
-            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            return Context.ConnectionId;
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await base.OnConnectedAsync();
+            Console.WriteLine("A client connected to ChatHub: " + GetConnectionId());
+        }
+        public override async Task OnDisconnectedAsync(Exception e)
+        {
+            await base.OnDisconnectedAsync(e);
+            Console.WriteLine("A client disconnected to ChatHub: " + e);
+        }
+
+        public async Task Send(string userId)
+        {
+            var message = $"Send message to you with user id {userId}";
+            await Clients.Client(userId).SendAsync("ReceiveMessage", message);
         }
     }
 }
--- SignalR/SignalR/Startup.cs
+++ SignalR/SignalR/Startup.cs
@@ -1,10 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SignalR.Hubs;
-
 
 namespace SignalR
 {
@@ -15,37 +15,38 @@ namespace SignalR
             Configuration = configuration;
         }
 
-        readonly string MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-           
-            services.AddControllers();
-            services.AddSignalR();
+
+                services.AddCors();
+                services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
+                services.AddControllers();
+                services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(
+                options => options.WithOrigins("http://localhost:3000")
+                .AllowAnyMethod().AllowAnyHeader().AllowCredentials()
+            );
+            app.UseRouting();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-         
-            app.UseHttpsRedirection();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
-
+            
             app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-                endpoints.MapHub<ChatHub>("/chatHub");
-            });
+           {
+               endpoints.MapControllers();
+
+               endpoints.MapHub<ChatHub>("ChatHub");
+           });
         }
     }
 }
